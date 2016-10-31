b2f32ce5bcde89df46313b803f53e82282d42a8e
I-Chiao Hsu <ichiao.hsu@gmail.com>
2016-10-31 16:37:02 +0800

    Introduce crispy_forms

--- draw_site/settings.py
+++ draw_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'draw_member',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
